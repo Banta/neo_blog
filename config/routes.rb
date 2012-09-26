aa74dc1b528c8ff52e48953e9f6e107e7cdbc7cb
Fabian Becker <halfdan@xnorfz.de>
2012-09-26 14:14:58 +0200

    Typo

--- config/routes.rb
+++ config/routes.rb
@@ -50,7 +50,7 @@ NeoBlog::Application.routes.draw do
 
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
-  root :to => 'post#index'
+  root :to => 'posts#index'
 
   # See how all your routes lay out with "rake routes"
 
